4c39052751292379994b1e5f70e0b130132e0030
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2019-01-11 21:49:44 +0600

    Registration System is fixed.Made Firebase database connected with ClassScedule.

---
+++ app/src/main/java/com/example/hp/swe/Batch_16.java
@@ -0,0 +1,13 @@
+package com.example.hp.swe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Batch_16 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batch_16);
+    }
+}
